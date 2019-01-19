e5470e6a27c0ddd657d6e520d76afa162953dcc7
Michael McDargh <mike@mcdargh.com>
2019-01-18 20:56:07 -0500

    using AdaFruit library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
 #include <Wire.h>
-#include "VEML6075.h"
 
+#include "Adafruit_VEML6075.h"
 #include "SSD1306.h"
 
 /* Place your settings in the settings.h file for ssid, password, and dweet post string
@@ -15,9 +16,10 @@ const char* dweet = "/dweet/for/myservice?uv=";
 #include "settings.h"
 
 // Host
-const char* host = "dweet.io";
+const char *host = "dweet.io";
 // Declare sensor controller instance
-VEML6075 my_veml6075 = VEML6075();
+Adafruit_VEML6075 my_veml6075 = Adafruit_VEML6075();
+bool wifiConnected = false;
 
 // OLED
 SSD1306 display(0x3c, D2, D1);
@@ -27,39 +29,50 @@ char _uv[32], _uva[32], _uvb[32];
 
 // forwards
 void connectToSerial(void);
-void connectToWifi(void);
+bool connectToWifi(void);
 bool initialize_VEML(void);
-void sendToDweet(const char* uvlevel);
+void sendToDweet(const char *uvlevel);
 void flashLED();
 void startOLED();
-void message(const char* msg);
+void message(const char *msg);
+void setupOTA(void);
 
 void setup()
 {
   // turn off LED
   digitalWrite(2, HIGH);
-  
+
   connectToSerial();
   startOLED();
-  connectToWifi();
+  if (connectToWifi())
+  {
+    setupOTA();
+  }
+  else
+  {
+    ESP.restart();
+  }
 
-   if(initialize_VEML() != true) {
-     message("Unable to initialize\n");
+  if (initialize_VEML() != true)
+  {
+    message("Unable to initialize\n");
+  }
 
-   }
-
-   // get led ready
-   pinMode(2, OUTPUT);
+  // get led ready
+  pinMode(2, OUTPUT);
 }
 
-void connectToSerial() {
+void connectToSerial()
+{
   Serial.begin(115200);
-  while(!Serial); //wait for serial port to connect (needed for Leonardo only)
+  while (!Serial)
+    ; //wait for serial port to connect (needed for Leonardo only)
   Wire.begin();
 }
 
-void startOLED() {
-// Initialize the OLED display using Wire library
+void startOLED()
+{
+  // Initialize the OLED display using Wire library
   display.init();
 
   display.flipScreenVertically();
@@ -70,48 +83,64 @@ void startOLED() {
   yield();
 }
 
-void connectToWifi() {
-  int numtries = 10;
+bool connectToWifi()
+{
+  int numtries = 20;
   // Connect to WiFi
-   WiFi.begin(ssid, password);
-   while (numtries-- && (WiFi.status() != WL_CONNECTED)) {
-     delay(500);
-     message(".");
-   }
-   message("\n");
-   message("WiFi connected\n");
+  WiFi.hostname(hostname);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  delay(500);
+  while (numtries-- && (WiFi.status() != WL_CONNECTED))
+  {
+    delay(750);
+    message(".");
+  }
 
-   // Print the IP address
-   message(WiFi.localIP().toString().c_str());
-   message("\n");
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    message("WIFI not connected\n");
+    return false;
+  }
+
+  message("\n");
+  message("WiFi connected\n");
+
+  // Print the IP address
+  message(WiFi.localIP().toString().c_str());
+  message("\n");
+
+  WiFi.setAutoReconnect(true);
+  wifiConnected = true;
+  return true;
 }
 
-
-bool initialize_VEML() {
+bool initialize_VEML()
+{
   // Initialize i2c bus and sensor
-  if (my_veml6075.begin()) {
-      message("Initialized successfully.\n");
-      return true;
-    }
+  if (!my_veml6075.begin())
+  {
+    Serial.println("Failed to communicate with VEML6075 sensor, check wiring?");
     return false;
+  }
+  Serial.println("Found VEML6075 sensor");
+  return true;
 }
 
 void loop()
 {
   char msg[256];
-  
-  //flashLED();
+
+  ArduinoOTA.handle();
+  flashLED();
 
   message("Taking reading...");
 
-  // Poll sensor
-  my_veml6075.poll();
-
   // Get "raw" UVA and UVB counts, with the dark current removed
-  float uva = my_veml6075.getUVA();
-  float uvb = my_veml6075.getUVB();
+  float uva = my_veml6075.readUVA();
+  float uvb = my_veml6075.readUVB();
   // Get calculated UV index based on Vishay's application note
-  float uv_index = my_veml6075.getUVIndex();
+  float uv_index = my_veml6075.readUVI();
 
   dtostrf(uva, 4, 2, _uva);
   dtostrf(uvb, 4, 2, _uvb);
@@ -124,18 +153,21 @@ void loop()
   delay(1000);
 }
 
-void flashLED(void) {
+void flashLED(void)
+{
   // flash led
-    digitalWrite(2, LOW);
-    delay(250);
-    digitalWrite(2, HIGH);
+  digitalWrite(2, LOW);
+  delay(250);
+  digitalWrite(2, HIGH);
 }
 
-void sendToDweet(const char* uvlevel) {
+void sendToDweet(const char *uvlevel)
+{
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
+  if (!client.connect(host, httpPort))
+  {
     message("connection to dweet failed\n");
     return;
   }
@@ -147,12 +179,14 @@ void sendToDweet(const char* uvlevel) {
   delay(10);
 
   // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
+  while (client.available())
+  {
     String line = client.readStringUntil('\r');
   }
 }
 
-void message(const char* pmsg) {
+void message(const char *pmsg)
+{
   char msg[128];
 
   // dump to serial
@@ -178,6 +212,64 @@ void message(const char* pmsg) {
 
   display.display();
   yield();
+}
+
+void setupOTA(void)
+{
+  ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(hostname);
+
+  // No authentication by default
+  ArduinoOTA.setPassword(otapwd);
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    flashLED();
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+#ifdef _DISPLAY_
+    if (displayConnected)
+    {
+      display.clear();
+      display.setTextAlignment(TEXT_ALIGN_CENTER);
+      display.setFont(ArialMT_Plain_10);
+      display.drawString(64, 10, "OTA Update");
+      display.drawProgressBar(2, 28, 124, 10, progress / (total / 100));
+      display.display();
+    }
+#endif
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR)
+      Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR)
+      Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR)
+      Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR)
+      Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR)
+      Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
 }
 
 // EOF
